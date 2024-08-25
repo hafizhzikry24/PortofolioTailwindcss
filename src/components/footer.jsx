b84e8972693724f0c62073aefe27b6e8f89dc23a
hafizhzikry24 <81897441+hafizhzikry24@users.noreply.github.com>
2024-08-25 17:53:20 +0700

    fix mobile page

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -52,7 +52,7 @@ const Footer = () => {
   }, []);
 
   return (
-    <footer className="bg-gray-900 text-white py-12 sm:py-8" id="contact">
+    <footer className="bg-gray-900 text-white py-12 sm:py-12" id="contact">
       <div className="container mx-auto flex flex-col md:flex-row justify-around items-center px-5">
         <div className="flex items-center mb-6 md:mb-0">
           <img
