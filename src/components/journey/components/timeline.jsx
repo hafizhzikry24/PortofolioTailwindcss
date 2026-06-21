fa1eb3bc9a7a16a8a4f4ff14eb794fc95a02e0ac
hafizhzikry24 <hafizhcool24@gmail.com>
2026-06-21 17:59:03 +0700

    fix:update experience

--- src/components/journey/components/timeline.jsx
+++ src/components/journey/components/timeline.jsx
@@ -49,27 +49,6 @@ export function TimelineDemo() {
       ),
     },
     {
-      title:
-        language === "en"
-          ? "Diponegoro University (Assistant Practicum)"
-          : "Universitas Diponegoro (Asisten Praktikum)",
-      category: "Education",
-      date: "2024-08-01",
-      content: (
-        <div>
-          <p className="text-purple-400 text-sm md:text-base font-medium mb-4 flex items-center">
-            <span className="inline-block w-2 h-2 rounded-full bg-purple-500 mr-2"></span>
-            Feb 2023 - April 2024
-          </p>
-          <p className="text-gray-300 text-sm md:text-base font-normal text-justify leading-relaxed">
-            {language === "en"
-              ? "During my studies I took the opportunity to become a practicum assistant, I taught: Assistant practicum of Digital System Class, Assistant practicum of Introduction To Network Class, Assistant practicum of Advanced Digital Systems Class, Assistant practicum of Switching, routing and wireless essentials Class, Assistant practicum of Automation and Control Systems Practical Class."
-              : "Selama studi saya, saya mengambil kesempatan untuk menjadi asisten praktikum, saya mengajar: Asisten praktikum Kelas Sistem Digital, Asisten praktikum Kelas Pengantar Jaringan, Asisten praktikum Kelas Sistem Digital Lanjut, Asisten praktikum Kelas Switching, routing dan wireless essentials, Asisten praktikum Kelas Sistem Otomasi dan Kontrol."}
-          </p>
-        </div>
-      ),
-    },
-    {
       title: "PT Awan Network Indonesia (Junior Backend Developer)",
       category: "Backend Development",
       date: "2024-08-01",
@@ -90,38 +69,20 @@ export function TimelineDemo() {
     {
       title:
         language === "en"
-          ? "MTQMN XVII at Malang (UI/UX Designer)"
-          : "MTQMN XVII Malang (Desain UI/UX)",
-      category: "Design",
-      date: "2023-11-01",
+          ? "Diponegoro University (Assistant Practicum)"
+          : "Universitas Diponegoro (Asisten Praktikum)",
+      category: "Education",
+      date: "2024-08-01",
       content: (
         <div>
           <p className="text-purple-400 text-sm md:text-base font-medium mb-4 flex items-center">
             <span className="inline-block w-2 h-2 rounded-full bg-purple-500 mr-2"></span>
-            Nov 2023
+            Feb 2023 - April 2024
           </p>
           <p className="text-gray-300 text-sm md:text-base font-normal text-justify leading-relaxed">
             {language === "en"
-              ? "Won first place in the Diponegoro University MTQM Al-Qur'an Application Design competition, representing the university at the National MTQM at Brawijaya University, Malang. Our team, out of 50 university representatives, presented the Quran Application Design and finished in the top 18."
-              : "Meraih juara pertama dalam kompetisi Desain Aplikasi Al-Qur'an MTQM Universitas Diponegoro, mewakili universitas di MTQM Nasional di Universitas Brawijaya, Malang. Tim kami, dari 50 perwakilan universitas, mempresentasikan Desain Aplikasi Quran dan selesai di peringkat 18 besar."}
-          </p>
-        </div>
-      ),
-    },
-    {
-      title: "SMKN 53 Jakarta (Network Engineer Internship)",
-      category: "Networking",
-      date: "2023-01-01",
-      content: (
-        <div>
-          <p className="text-purple-400 text-sm md:text-base font-medium mb-4 flex items-center">
-            <span className="inline-block w-2 h-2 rounded-full bg-purple-500 mr-2"></span>
-            Jan 2023 - Feb 2023
-          </p>
-          <p className="text-gray-300 text-sm md:text-base font-normal text-justify leading-relaxed">
-            {language === "en"
-              ? "Carrying out network design or what is known as network architecture, installing and configuring the network design then monitoring the network to ensure that the network does not experience problems, troubleshooting which is carried out when interference or problems are found on a network and documentation is useful for reports and also makes the subsequent troubleshooting process easier."
-              : "Melakukan perancangan jaringan atau yang dikenal dengan arsitektur jaringan, memasang dan mengkonfigurasi desain jaringan kemudian memantau jaringan untuk memastikan bahwa jaringan tidak mengalami masalah, pemecahan masalah yang dilakukan saat gangguan atau masalah ditemukan pada jaringan dan dokumentasi berguna untuk laporan dan juga mempermudah proses pemecahan masalah selanjutnya."}
+              ? "During my studies I took the opportunity to become a practicum assistant, I taught: Assistant practicum of Digital System Class, Assistant practicum of Introduction To Network Class, Assistant practicum of Advanced Digital Systems Class, Assistant practicum of Switching, routing and wireless essentials Class, Assistant practicum of Automation and Control Systems Practical Class."
+              : "Selama studi saya, saya mengambil kesempatan untuk menjadi asisten praktikum, saya mengajar: Asisten praktikum Kelas Sistem Digital, Asisten praktikum Kelas Pengantar Jaringan, Asisten praktikum Kelas Sistem Digital Lanjut, Asisten praktikum Kelas Switching, routing dan wireless essentials, Asisten praktikum Kelas Sistem Otomasi dan Kontrol."}
           </p>
         </div>
       ),
